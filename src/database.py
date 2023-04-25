71680ca496d76280a885fa742fc86fa074bc8b2f
Abbazov Valerian <asr77734@gmail.com>
2023-04-25 09:48:57 +0500

    lab_11

--- src/database.py
+++ src/database.py
@@ -2,7 +2,15 @@ from os import environ
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = environ.get('DATABASE_URL')
+
+DB_USER = environ.get('DB_USER').strip()
+DB_PASSWD = environ.get('DB_PASSWD').strip()
+DB_NAME = environ.get('DB_NAME').strip()
+DB_HOST = environ.get('DB_HOST').strip()
+
+print(f"{DB_USER=}, {DB_PASSWD=}, {DB_NAME=}, {DB_HOST=}")
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWD}@{DB_HOST}:5432/{DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
